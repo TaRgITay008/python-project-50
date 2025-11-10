9f6b99013cafb723ec2a60b29e8f6069f501d66f
Targitay <cibrenok@bk.ru>
2025-11-10 22:26:57 +0300

    Add format option to gendiff script

--- src/hexlet_code/scripts/gendiff.py
+++ src/hexlet_code/scripts/gendiff.py
@@ -2,10 +2,10 @@
 import argparse
 
 
-def generate_diff(file1, file2):
+def generate_diff(file1, file2, format_name='stylish'):
     """Generate diff between two files."""
     # Пока заглушка - реализуем на следующих шагах
-    return f"Diff between {file1} and {file2}"
+    return f"Diff between {file1} and {file2} in {format_name} format"
 
 
 def main():
@@ -14,13 +14,15 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+                        default='stylish',
+                        help='set format of output')
     
     args = parser.parse_args()
     
-    diff = generate_diff(args.first_file, args.second_file)
+    diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
 
 
 if __name__ == '__main__':
     main()
-
