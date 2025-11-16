0d2d1a585cd931bbd0a624a1ebed61e78425b525
Targitay <cibrenok@bk.ru>
2025-11-16 16:49:43 +0300

    Add

--- gendiff/formatters/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Formatters module."""
-from .stylish import format_stylish
-from .plain import format_plain
-from .json import format_json
-
-__all__ = ['format_stylish', 'format_plain', 'format_json']
--- gendiff/formatters/json.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""JSON formatter module."""
-import json
-
-
-def format_json(diff):
-    """Format diff in JSON format."""
-    return json.dumps(diff, indent=2)
