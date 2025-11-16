d7ef81a60c339a0ab27753ec26d43f178ad98aae
Targitay <cibrenok@bk.ru>
2025-11-16 16:51:35 +0300

    Add

---
+++ hexlet_code/formatters/__init__.py
@@ -0,0 +1,6 @@
+"""Formatters module."""
+from .stylish import format_stylish
+from .plain import format_plain
+from .json import format_json
+
+__all__ = ['format_stylish', 'format_plain', 'format_json']
