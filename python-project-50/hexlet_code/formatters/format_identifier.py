f75f5193c06e99d0be09e1e7cd88680c76f60261
Targitay <cibrenok@bk.ru>
2025-11-25 22:58:20 +0300

    Add

---
+++ python-project-50/hexlet_code/formatters/format_identifier.py
@@ -0,0 +1,14 @@
+from hexlet_code.formatters.json_formatter import format_diff_json
+from hexlet_code.formatters.plain import format_diff_plain
+from hexlet_code.formatters.stylish import format_diff_stylish
+
+
+def format_identifier(diff, formatter):
+    if formatter == 'stylish':
+        return format_diff_stylish(diff)
+    if formatter == 'plain':
+        return format_diff_plain(diff)
+    if formatter == 'json':
+        return format_diff_json(diff)
+    else:
+        raise ValueError(f"Unsupported formatter: {formatter}")
---
+++ python-project-50/tests/test_gendiff.py
@@ -0,0 +1,43 @@
+import pytest
+
+from hexlet_code.scripts.generate_diff import generate_diff
+from hexlet_code.scripts.parser import read_file
+
+
+@pytest.mark.parametrize('file_path1, file_path2, expected_result', [
+    ('tests/test_data/file1.json',
+     'tests/test_data/file2.json',
+     'tests/test_data/expected_result_json.txt'),
+    ('tests/test_data/file1.yaml',
+     'tests/test_data/file2.yaml',
+     'tests/test_data/expected_result_yaml.txt')])
+def test_generate_diff(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2)
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
+
+
+@pytest.mark.parametrize('file_path1, file_path2, expected_result', [
+    ('tests/test_data/file1.json',
+     'tests/test_data/file2.json',
+     'tests/test_data/expected_result_plain.txt'),
+    ('tests/test_data/file1.yaml',
+     'tests/test_data/file2.yaml',
+     'tests/test_data/expected_result_plain.txt')])
+def test_generate_diff_plain(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2, formatter="plain")
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
+
+
+@pytest.mark.parametrize('file_path1, file_path2, expected_result', [
+    ('tests/test_data/file1.json',
+     'tests/test_data/file2.json',
+     'tests/test_data/expected_result_json_format.txt'),
+    ('tests/test_data/file1.yaml',
+     'tests/test_data/file2.yaml',
+     'tests/test_data/expected_result_json_format.txt')])
+def test_generate_diff_json(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2, formatter="json")
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
